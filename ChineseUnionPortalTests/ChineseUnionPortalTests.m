5d77f1e1173104f4dc6dd26ce94b8d24fddc3d4b
Wei-Ping-Liao <wpliao1989@gmail.com>
2014-05-24 16:06:22 -0700

    Initial Commit

---
+++ ChineseUnionPortalTests/ChineseUnionPortalTests.m
@@ -0,0 +1,34 @@
+//
+//  ChineseUnionPortalTests.m
+//  ChineseUnionPortalTests
+//
+//  Created by wpliao on 2014/5/24.
+//  Copyright (c) 2014年 ucsd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ChineseUnionPortalTests : XCTestCase
+
+@end
+
+@implementation ChineseUnionPortalTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
